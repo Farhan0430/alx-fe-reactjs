7b439203d061e993af689c26cc229f6bad6acb66
Farhan0430 <farhan.sidik66@gmail.com>
2025-08-03 09:20:08 +0200

    Creating API

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,16 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://api.github.com';
+
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`${BASE_URL}/users/${username}`, {
+    headers: {
+      Authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
+    },
+  });
+  return response.data;
+};
+
 export const searchUsers = async ({ username, location, minRepos }) => {
   let query = '';
 
@@ -7,7 +18,7 @@ export const searchUsers = async ({ username, location, minRepos }) => {
   if (location) query += `location:${location} `;
   if (minRepos) query += `repos:>=${minRepos}`;
 
-  const url = `https://api.github.com/search/users?q=${encodeURIComponent(query)}`;
+  const url = `${BASE_URL}/search/users?q=${encodeURIComponent(query)}`;
 
   const response = await axios.get(url, {
     headers: {
