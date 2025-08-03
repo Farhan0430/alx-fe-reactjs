e67348bdd29763fb64d2c3f82d0a1696816c1236
Farhan0430 <farhan.sidik66@gmail.com>
2025-08-03 09:03:51 +0200

    Creating API

--- github-user-search/src/components/Placeholder.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/Placeholder.jsx
-import React from 'react';
-
-const Placeholder = () => {
-  return <div>Component placeholder</div>;
-};
-
-export default Placeholder;
---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const fetchUserData = async (username) => {
+  const url = `https://api.github.com/users/${username}`;
+
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
+    },
+  });
+
+  return response.data;
+};
