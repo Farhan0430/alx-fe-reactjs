75d9627003d62910f6f61575aeb46535a9cf2f8d
Farhan0430 <farhan.sidik66@gmail.com>
2025-08-13 21:20:23 +0200

    Recipe Sharing Platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-4xl font-extrabold text-green-600 bg-yellow-200 p-4 rounded-lg">
+        Tailwind CSS is Working!
+      </h1>
+    </div>
+  );
+}
